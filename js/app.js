6cb0a236ee010a410b9ae6871685956464a3664e
Bpost129 <Bpost129@yahoo.com>
2024-03-30 21:20:18 -0400

    add to wager total on button click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log("blackjack!!")
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let wagerTotal = 0
+
+/*------------------------ Cached Element References ------------------------*/
+
+const wagerBtns = [...document.querySelectorAll('.wager-buttons')]
+const wagerEl = document.querySelector('.wager-display')
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+wagerBtns.forEach(wagerBtn => {
+  wagerBtn.addEventListener('click', handleWager)
+})
+
+/*-------------------------------- Functions --------------------------------*/
+
+function handleWager(e) {
+  const btnIdx = Math.floor(e.target.id.substring(3))
+  // Number(+btnIdx)
+  console.log(btnIdx)
+  wagerTotal += btnIdx
+  wagerEl.textContent = `$${wagerTotal}`
+}
