76f5c00dda717deec48d1b7f749df5bda0230a4b
Bpost129 <Bpost129@yahoo.com>
2024-03-30 21:33:20 -0400

    work on deal functionality with total earnings, should subtract wager from total

--- js/app.js
+++ js/app.js
@@ -4,25 +4,38 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let totalEarnings = 100
 let wagerTotal = 0
 
 /*------------------------ Cached Element References ------------------------*/
 
 const wagerBtns = [...document.querySelectorAll('.wager-buttons')]
 const wagerEl = document.querySelector('.wager-display')
+const earningsEl = document.querySelector('.total')
+const dealBtn = document.querySelector('#deal')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 wagerBtns.forEach(wagerBtn => {
   wagerBtn.addEventListener('click', handleWager)
 })
+dealBtn.addEventListener('click', handleDeal)
 
 /*-------------------------------- Functions --------------------------------*/
 
+earningsEl.textContent = `$${totalEarnings}`
+
+// function init() {
+//   earningsEl.textContent = `$${totalEarnings}`
+// }
+
 function handleWager(e) {
   const btnIdx = Math.floor(e.target.id.substring(3))
-  // Number(+btnIdx)
-  console.log(btnIdx)
   wagerTotal += btnIdx
   wagerEl.textContent = `$${wagerTotal}`
+}
+
+function handleDeal() {
+  console.log('deal!')
+  totalEarnings -= wagerTotal
 }
